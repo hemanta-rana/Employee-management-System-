4a50ceaf35026c6f0b9fc1889d6599cc1299c88b
Hemanta Rana <magarhemanta145@gmail.com>
2026-02-26 22:15:26 +0545

    cusomize securityfilter chain

--- src/main/java/com/project/EMS/security/config/SecurityConfig.java
+++ src/main/java/com/project/EMS/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.EMS.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,8 +21,10 @@ public class SecurityConfig {
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> {
                     request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+                    request.requestMatchers(HttpMethod.POST, "/api/v1/users/register").permitAll();
                     request.anyRequest().authenticated();
                 });
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
