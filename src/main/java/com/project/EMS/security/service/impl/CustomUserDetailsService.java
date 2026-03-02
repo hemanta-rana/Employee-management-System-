d218069f2fa17c3c39044b730f3c2520942f7c98
Hemanta Rana <magarhemanta145@gmail.com>
2026-03-02 10:11:37 +0545

    updated user related service

---
+++ src/main/java/com/project/EMS/security/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.project.EMS.security.service.impl;
+
+import com.project.EMS.exception.ResourceNotFoundException;
+import com.project.EMS.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()-> new ResourceNotFoundException("Username not found with name :"+username));
+    }
+}
